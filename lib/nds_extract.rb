ec5a0c403ad8be0fea04f5763ea768573b79434e
Melike KILIC <melike180400@gmail.com>
2020-01-22 02:56:52 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,6 +2,14 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
+   row_index=0
+   while row_index< directors_database.length do
+    puts "#{directors_database[:name][:worlwide_gross]}"
+    row_index+=1
+   end
+
+
+
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #
@@ -18,5 +26,4 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
 end
